58d33965f066bd053f28830289b8f98f2dae256a
xuzheng <xuzheng884775@gmail.com>
2026-06-11 16:28:56 +0800

    initial commit

---
+++ helpers.py
@@ -0,0 +1,96 @@
+import re 
+import requests
+import dateparser
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+gmaps_API = os.getenv("gmaps_API")
+
+def get_coords(url: str):
+    patterns = [
+        r"!3d(-?\d+\.\d+)!4d(-?\d+\.\d+)",
+        r"@(-?\d+\.\d+),(-?\d+\.\d+)",         
+        r"[?&]q=(-?\d+\.\d+),(-?\d+\.\d+)",      
+        r"[?&]ll=(-?\d+\.\d+),(-?\d+\.\d+)",      
+        r"(-?\d+\.\d+),\+(-?\d+\.\d+)",
+    ]
+
+    for pattern in patterns:
+        valid = re.search(pattern, url)
+        
+        if valid:
+            return float(valid.group(1)), float(valid.group(2))
+        
+    return None
+
+def get_placename(url: str):
+    match = re.search(r"place/([^/]+)/", url)
+    if match:
+        placename = match.group(1)
+        return (placename.replace("+"," "))
+    return None
+
+
+def get_travel_time(origin_lat, origin_lon, mode, dest_placeid = None, dest_lat = None, dest_lon = None):
+    url = "https://routes.googleapis.com/directions/v2:computeRoutes"
+    
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": gmaps_API,
+        "X-Goog-FieldMask": "routes.duration"  
+    }
+    
+    body = {
+        "origin": {"location": {"latLng": {"latitude": origin_lat, "longitude": origin_lon}}},
+        "destination": {},
+        "travelMode": mode.upper()  # DRIVE, TRANSIT, WALK
+    }
+    if dest_placeid:
+        body["destination"] = {"placeId": dest_placeid}
+    else:
+        body["destination"] = {"location": {"latLng": {"latitude": dest_lat, "longitude": dest_lon}}}
+    response = requests.post(url, json=body, headers=headers)
+    data = response.json()
+    
+    if "routes" in data and data["routes"]:
+        seconds = int(data["routes"][0]["duration"].rstrip("s"))
+        return round(seconds / 60)
+    return None
+
+def meetup_time(time):
+    dt = dateparser.parse(time, settings={
+        "PREFER_DATES_FROM": "future",
+        "TIMEZONE": "Asia/Singapore",
+        "DATE_ORDER": "DMY"
+    })
+    return dt
+
+def get_placeid(input: str):
+    url = "https://places.googleapis.com/v1/places:searchText"
+    
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": gmaps_API,
+        "X-Goog-FieldMask": "places.id,places.displayName,places.location"
+    }
+    
+    body = {
+        "textQuery": input
+    }
+    
+    response = requests.post(url, json=body, headers=headers)
+    data = response.json()
+    
+    if "places" in data and data["places"]:
+        temp = {"placeid" : data["places"][0]["id"],
+                "name" : data["places"][0]["displayName"]["text"],
+                "lat" : data["places"][0]["location"]["latitude"],
+                "long" : data["places"][0]["location"]["longitude"]}
+        return temp
+    return None
+
+pictures = ["https://raw.githubusercontent.com/xu-zh3ng/stopbeinglate_bot/main/images/images%20(1).jpeg",
+            "https://raw.githubusercontent.com/xu-zh3ng/stopbeinglate_bot/main/images/images%20(2).jpeg",
+            "https://raw.githubusercontent.com/xu-zh3ng/stopbeinglate_bot/main/images/images%20(3).jpeg",
+            "https://raw.githubusercontent.com/xu-zh3ng/stopbeinglate_bot/main/images/images%20(4).jpeg"]
---
+++ main.py
@@ -0,0 +1,315 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, PicklePersistence
+import requests
+from datetime import datetime, timedelta
+from helpers import get_coords, get_travel_time, meetup_time, get_placename, get_placeid, pictures
+import os
+from dotenv import load_dotenv
+import random
+import re
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
+wayne_id = int(os.getenv("wayne_id"))
+rayen_id = int(os.getenv("rayen_id"))
+yukai_id = int(os.getenv("yukai_id"))
+charis_id = int(os.getenv("charis_id"))
+anjoe_id = int(os.getenv("anjoe_id"))
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "/setpin (google maps link OR location name) : Set meeting location\n"
+        "/clear : Clears previous meeting location \n"
+        "/mode : Changes your mode of transport \n"
+        "/meetup : Check your meet up timings, if any"
+        "/setmeetup : Set a date and time to meet up \n"
+        "After setting a location, simply send your own location to receive your travel time \n"
+        "Reply 'leavetime' after setting a meet up to know what time you should leave"
+    )
+
+async def setpin_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #check if user sent a link after /setpin
+    if not context.args:
+        await update.message.reply_text(
+            "Usage: /setpin (google maps link)"
+        )
+        return
+
+    #user has sent something after /setpin -> check if url/valid loc
+    input = " ".join(context.args)
+    if re.match(r"https?://", input):
+        #url checking
+        response = requests.get(input, allow_redirects=True)  
+        coords = get_coords(response.url)
+        placename = get_placename(response.url)
+        if not coords:
+            await update.message.reply_text("Couldn't find that location.")
+            print("error 1")
+            return
+        #if there is a placename -> search for placeid
+        if placename: 
+            data = get_placeid(placename)
+            if not data:
+                await update.message.reply_text("Couldn't find that location.")
+                print("error 2")
+                return
+            context.chat_data["pinned_location"] = {"lat": data["lat"], "long": data["long"], "place_id": data["placeid"], "name": data["name"]}
+            await update.message.reply_text(f"Location saved at {data['name']}")
+            return
+        #else no placename in url
+        else:
+            context.chat_data["pinned_location"] = {"lat": coords[0], "long": coords[1], "place_id": None, "name": None}
+            await update.message.reply_text(f"Location saved at {coords[0]} , {coords[1]}")
+            return
+
+    else:
+        #is place name --> do placeid search
+        data = get_placeid(input)
+        if not data:
+            await update.message.reply_text("Couldn't find that location.")
+            print("error 3")
+            return
+        context.chat_data["pinned_location"] = {"lat": data["lat"], "long": data["long"], "place_id": data["placeid"], "name": data["name"]}
+        url = f"https://www.google.com/maps/search/?api=1&query={data['name']}&query_place_id={data['placeid']}"
+        await update.message.reply_text(f"Location saved at {data['name']}",
+                                        reply_markup=InlineKeyboardMarkup([[
+                                        InlineKeyboardButton("Open in Google Maps", url=url)
+                                        ]]))
+
+        return
+
+async def clear_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        await update.message.reply_text("Usage: /clear (pinned/meetup/all)")
+        return
+    
+    content = context.args[0]
+    if content not in ["pinned", "meetup", "all"]:
+        await update.message.reply_text("Usage: /clear (pinned/meetup/all)")
+        return
+    
+    if content == "pinned":
+        context.chat_data.pop("pinned_location", None)
+        await update.message.reply_text("Location deleted!")
+        return
+
+    elif content == "meetup":
+        context.chat_data.pop("meetup_time", None)
+        await update.message.reply_text("Meetup deleted!")
+        return
+    
+    elif content == "all":
+        context.chat_data.pop("meetup_time", None)
+        context.chat_data.pop("pinned_location", None)
+        await update.message.reply_text("All cleared!")
+
+async def setmeetup_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if "pinned_location" not in context.chat_data:
+        await update.message.reply_text("No pinned location. \n"
+                                        "Use /setpin to pin a location")
+        return
+    time = meetup_time("".join(context.args))
+    if not time:
+        await update.message.reply_text("Invalid date/time format")
+        return
+    
+    context.chat_data["meetup_time"] = time
+    await update.message.reply_text(f"Meet up set on {time.strftime("%d %B %Y at %I:%M%p")} \n"
+                                    "Reply 'leavetime' to know what time you need to leave!")
+    
+async def meetup_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if "meetup_time" not in context.chat_data:
+        await update.message.reply_text("No meetup set. \n"
+                                        "Use /setmeetup to set a date and time")
+        return
+    
+    meetup = context.chat_data.get("meetup_time")
+    location = context.chat_data.get("pinned_location")
+    await update.message.reply_venue(latitude=location["lat"], longitude=location["long"], title=location["name"], address=f"Meet on {meetup.strftime("%d %B %Y at %I:%M%p")}")
+    
+async def user_loc(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if "pinned_location" not in context.chat_data:
+        await update.message.reply_text("No pinned location. \n"
+                                        "Use /setpin to pin a location")
+        return
+    
+    lat = update.message.location.latitude
+    long = update.message.location.longitude
+
+    context.user_data["current_location"] = {"lat": lat , "long": long}
+    mode = "transit"
+    context.user_data["mode"] = mode
+    pinned = context.chat_data.get("pinned_location") #read
+    if not pinned["place_id"]:
+        travel_time = get_travel_time(lat, long, mode, dest_lat=pinned["lat"], dest_lon=pinned["long"])
+        url = f"https://www.google.com/maps/dir/?api=1&origin={lat},{long}&destination={pinned['lat']},{pinned['long']}&travelmode={mode}"
+    else:
+        travel_time = get_travel_time(lat, long, mode, dest_placeid=pinned["place_id"])
+        url = f"https://www.google.com/maps/dir/?api=1&origin={lat},{long}&destination={pinned['lat']},{pinned['long']}&destination_place_id={pinned['place_id']}&travelmode={mode}"
+    context.user_data["travel_time"] = travel_time
+    hrs = int(travel_time / 60)
+    mins = travel_time % 60
+    if hrs == 0:
+        time = f"{mins}mins"
+    elif mins == 0:
+        time = f"{hrs}hrs"
+    else:
+        time = f"{hrs}hrs {mins}mins"
+    arrival_time = datetime.now() + timedelta(minutes=travel_time)
+    await update.message.reply_text(f"You are {time} away and will arrive at {arrival_time.strftime('%H:%M')}",
+                                    reply_markup=InlineKeyboardMarkup([[
+                                    InlineKeyboardButton("Open in Google Maps", url=url)
+                                    ]]))
+
+async def changemode_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        await update.message.reply_text(
+            "Usage: /mode (drive/transit/walk)"
+        )
+        return
+    
+    mode = context.args[0]
+    mode = mode.lower()
+
+    if mode not in ["drive" , "transit", "walk"]:
+        await update.message.reply_text(
+            "Usage: /mode (drive/transit/walk)"
+        )
+        return
+    
+    context.user_data["mode"] = mode
+
+    if "pinned_location" not in context.chat_data:
+        await update.message.reply_text("No pinned location. \n"
+                                        "Use /setpin to pin a location")
+        return
+    if "current_location" not in context.user_data:
+        await update.message.reply_text("No user location. \n"
+                                        "Use /setpin to pin a location")
+        return
+    
+    current = context.user_data.get("current_location")
+    pinned = context.chat_data.get("pinned_location")
+    if not pinned["place_id"]:
+        travel_time = get_travel_time(current["lat"], current["long"], mode, dest_lat=pinned["lat"], dest_lon=pinned["long"])
+    else:
+        travel_time = get_travel_time(current["lat"], current["long"], mode, dest_placeid=pinned["place_id"])
+    
+    if mode == "drive":
+        url_mode = "driving"
+    elif mode == "walk":
+        url_mode = "walking"
+    else: url_mode == mode
+
+    context.user_data["travel_time"] = travel_time
+    arrival_time = datetime.now() + timedelta(minutes=travel_time)
+    hrs = int(travel_time / 60)
+    mins = travel_time % 60
+    if hrs == 0:
+        time = f"{mins}mins"
+    elif mins == 0:
+        time = f"{hrs}hrs"
+    else:
+        time = f"{hrs}hrs {mins}mins"
+    url = f"https://www.google.com/maps/dir/?api=1&origin={current['lat']},{current['long']}&destination={pinned['lat']},{pinned['long']}&travelmode={url_mode}"
+
+    await update.message.reply_text(f"You are {time} away and will arrive at {arrival_time.strftime('%H:%M')}",
+                                    reply_markup=InlineKeyboardMarkup([[
+                                    InlineKeyboardButton("Open in Google Maps", url=url)
+                                    ]]))
+
+async def leavetime(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = update.message
+    if not message.reply_to_message:
+        return
+    if message.reply_to_message.from_user.id != context.bot.id:
+        return
+    
+    travel_time = context.user_data.get("travel_time")
+    meetup = context.chat_data.get("meetup_time")
+    if not meetup or not travel_time:
+        await update.message.reply_text("Missing meetup/travel time")
+        return
+    leave_time = meetup - timedelta(minutes=travel_time)
+    await update.message.reply_text(f"You should leave at {leave_time.strftime('%d %B %Y at %I:%M%p')} to reach at {meetup.strftime('%I:%M%p')}")
+    
+async def jay(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.delete_message(
+        chat_id=update.effective_chat.id,
+        message_id=update.message.message_id
+    )
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Message eaten by jay")
+    await context.bot.send_photo(chat_id=update.effective_chat.id, photo="photos/jay.jpg")
+
+async def trolling(update:Update, context: ContextTypes.DEFAULT_TYPE):
+    if not update.message:
+        return
+    n = random.randrange(1,10)
+    if update.message.from_user.id == yukai_id:
+        if n % 10 == 0:
+            await update.message.reply_photo(photo="photos/rainbow.png", caption="guy above looking for cock")
+    
+    elif update.message.from_user.id == charis_id:
+        if n < 3:
+            picture = random.choice(pictures)
+            await update.message.reply_photo(photo=picture, caption="meal time!")
+
+    elif update.message.from_user.id == anjoe_id:
+        if n < 3:
+            await context.bot.delete_message(chat_id=update.effective_chat.id,
+                                            message_id=update.message.message_id)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="chong ray en request")
+            await context.bot.send_photo(chat_id=update.effective_chat.id, photo="photos/decree.png")
+
+        elif n > 7:
+            await update.message.reply_photo(photo="photos/slander.png")
+
+async def catch_word(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.from_user.id == wayne_id and any(word in update.message.text.lower() for word in ["spurs", "knicks","work"]):
+        await update.message.reply_photo(photo="photos/larp.png")
+        return
+
+    elif update.message.from_user.id == rayen_id and "🫩" in update.message.text:
+        if "count" not in context.user_data:
+            context.user_data["count"] = 0
+
+        count = context.user_data.get("count")
+        count += 1
+        context.user_data["count"] = count
+
+        await update.message.reply_text(f"🫩 count:{count}")
+        return
+    
+    elif update.message.from_user.id == rayen_id and "me" in update.message.text:
+        await update.message.reply_photo(photo="photos/dude.png")
+        return
+
+def main():
+    persistence = PicklePersistence(filepath="bot_data.pkl")
+    app = ApplicationBuilder().token(TOKEN).persistence(persistence).build()
+    
+
+    app.add_handler(CommandHandler("help", help_command))
+    app.add_handler(CommandHandler("setpin", setpin_command))
+    app.add_handler(CommandHandler("clear", clear_command))
+    app.add_handler(CommandHandler("mode", changemode_command))
+    app.add_handler(CommandHandler("setmeetup", setmeetup_command))
+    app.add_handler(CommandHandler("meetup", meetup_command))
+    app.add_handler(MessageHandler(filters.LOCATION, user_loc))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex(r"(?i)spurs|knicks|🫩|me|work"), catch_word))
+    app.add_handler(MessageHandler(filters.TEXT & filters.REPLY & filters.Regex(r"(?i)leavetime"), leavetime))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex(r"(?i)jay"), jay))
+    app.add_handler(MessageHandler(filters.ALL, trolling))
+
+
+    app.run_polling(drop_pending_updates=True)
+
+
+
+
+
+if __name__ == "__main__":
+
+    main()
